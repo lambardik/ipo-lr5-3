4a50c458eb443bec3fe1bd0f122d11b18c9e0d24
даниэль <dannilboyar@gmail.com>
2024-11-16 09:23:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-filename = 'text.txt'
-with open(filename, 'r') as file:
-  text = file.read()
-  words = text.split()
-  word_count = len(words)
-print(f"В файле '{filename}' {word_count} слов.")
+string = open('text.txt','r').read()
+string = string.split() 
+print("Кол-во слов:",len(string))
+string.close()
